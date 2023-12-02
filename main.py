d6d4bb157425f1bd026b33cc2434098777b09d22
Kave Haj <kave.haj@snapp.cab>
2023-12-02 23:16:21 +0330

    added product weight for pricing

--- main.py
+++ main.py
@@ -1,75 +1,79 @@
-import random
 import pandas as pd
+import operator
 
 # Define your data
-products = {
-   'اکسیدان': 95871,
-   'ژل مو': 268814,
-   'ماسک مو': 907951,
-   'واکس مو': 349019,
-   'شامپو رنگ': 699918,
-   'نرم کننده مو': 323955,
-   'کرم دست و صورت': 356539,
-   'شامپو': 256282,
-   'شیر مو': 681120,
-   'بلک ماسک': 430478,
-   'ریممور': 828398,
-   'رنگ مو':231217
-}
+# Create a list of products with their weight coefficients
+products = [
+   {'name': 'اکسیدان', 'price': 95871, 'weight': 40},
+   {'name': 'ژل مو', 'price': 268814, 'weight': 20},
+   {'name': 'ماسک مو', 'price': 907951, 'weight': 10},
+   {'name': 'واکس مو', 'price': 349019, 'weight': 7},
+   {'name': 'شامپو رنگ', 'price': 699918, 'weight': 5},
+   {'name': 'نرم کننده مو', 'price': 323955, 'weight': 5},
+   {'name': 'کرم دست و صورت', 'price': 323955, 'weight': 5},
+   {'name': 'شامپو', 'price': 256282, 'weight': 3},
+   {'name': 'شیر مو', 'price': 681120, 'weight': 3},
+   {'name': 'بلک ماسک', 'price': 430478, 'weight': 1},
+   {'name': 'ریممور', 'price': 828398, 'weight': 0.5},
+   {'name': 'رنگ مو', 'price': 231217, 'weight': 0.5},
+]
+
+products = sorted(products, key=operator.itemgetter('price'))
 
 # Get user input for total price
 total_price = int(input("Enter the total price: "))
-
 # Initialize an empty list to store the combinations
+xlsx_name = "combination"+str(total_price/10000000)+".xlsx"
 combinations = []
 
-# While the total price is greater than the minimum product price
-while total_price > min(products.values()):
-   # Define the weights for each product
-   weights = [0.6, 0.2, 0.1] + [0.01] * (len(products) - 3)
+total_weight = 0
+for product in products:
+   total_weight += product['weight']
 
-   # Choose a random product
-   product, price = random.choices(list(products.items()), weights=weights)[0]
+total_weights = total_price/total_weight;
+for product in products:
+    # For each product
+    product_share = total_weights*product['weight']
+    product_count = int(product_share/product['price'])
 
-   # If the chosen price is greater than the total price, continue with the next iteration
-   if price > total_price:
-       continue
+    # If max_count is 0, continue with the next product
+    if product_count == 0:
+        continue
 
-   # Calculate the maximum possible count for the chosen price
-   max_count = total_price // price
+    # Subtract the total cost of these products from the total price
+    total_price -= product['price'] * product_count
 
-   # Choose a random count up to the maximum
-   count = random.randint(1, max_count)
+    # Add the combination to the list
+    combinations.append({'name': product['name'], 'price': product['price'], 'count': product_count, 'total_price': product['price']*product_count})
 
+# If the total price for this combination is greater than the minimum product price, add it to the combinations
+for combination in combinations:
+    if total_price > combination['price']:
+        product_count = int(total_price/combination['price'])
+        product_count += combination['count']
+        new_combination = {'name': combination['name'], 'price': combination['price'], 'count': product_count, 'total_price': combination['price']*product_count}
+        combination.update(new_combination)
 
+if total_price > 0:
+    combinations.append({'name': 'تخفیف', 'price': total_price, 'count': 1, 'total_price':total_price})
 
-   # Subtract the total cost of these products from the total price
-   total_price -= price * count
-
-   # Add the combination to the list
-   combinations.append((product, price, count))
-
-   # Remove the chosen product from the products dictionary
-   del products[product]
-
-combinations.append(("تخفیف", total_price, 1))
-
-# Print the combinations
-for product, price, count in combinations:
-   print("Product: ", product, "Price: ", price, "Count: ", count)
+for combination in combinations:
+    print(combination)
 
 # Convert the combinations to a pandas DataFrame
-df = pd.DataFrame(combinations, columns=['Product', 'Price', 'Count'])
+df = pd.DataFrame.from_dict(combinations)
 
 # Rename the DataFrame columns
 df = df.rename(columns={
-   'Product': 'sstt شرح کالا / خدمت',
-   'Price': 'fee مبلغ واحد',
-   'Count': 'am تعداد / مقدار',
+'Product': 'sstt شرح کالا / خدمت',
+'Price': 'fee مبلغ واحد',
+'Count': 'am تعداد / مقدار',
+'Total Price': 'مجموع کل',
 })
 
 # Define the columns you want to write to Excel
-columns = ['sstt شرح کالا / خدمت', 'fee مبلغ واحد', 'am تعداد / مقدار']
+columns = ['sstt شرح کالا / خدمت', 'fee مبلغ واحد', 'am تعداد / مقدار','مجموع کل']
 
 # Write the DataFrame to an Excel file
-df.to_excel('C:/Users/z.mehrasa/Desktop/invoice/combinations.xlsx', index=False)
+# df.to_excel('C:/Users/z.mehrasa/Desktop/invoice/combinations.xlsx', index=False)
+df.to_excel(xlsx_name, index=False)
